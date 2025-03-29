3124d784847814a9603b37d6d97c8c1be84416f2
CarlosEduardo113 <ce219560@gmail.com>
2025-03-29 20:39:55 -0300

    Finalizar Programa

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿        // Cebolinha - Exercício 2 da Lista 2
+
+        Console.Write("Digite uma frase qualquer: ");
+        string frase = Console.ReadLine();
+
+        string falaCebolinha = frase.Replace("r", "l").Replace("R", "L");
+
+        Console.WriteLine("\nCebolinha falando: " + falaCebolinha);
